cef1bcfcf462f82154da31757a519dc831a310f1
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-04-24 01:07:55 +0100

    cahnge with mock

--- config/config.exs
+++ config/config.exs
@@ -10,13 +10,27 @@ use Mix.Config
 config :github_repos,
   ecto_repos: [GithubRepos.Repo]
 
+config :github_repos, GithubRepos.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
+config :github_repos, GithubReposWeb.Auth.Guardian,
+  issuer: "github",
+  secret_key: "XWWyGS+0mCTgkR9zEIXqRQWk3nzcw4uxW/u5MMD2rEo5/ADeIZK33UH+338jArXO"
+
+config :github_repos, GithubReposWeb.Auth.Pipeline,
+  module: GithubReposWeb.Auth.Guardian,
+  error_handler: GithubReposWeb.Auth.ErrorHandler
+
 # Configures the endpoint
 config :github_repos, GithubReposWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "fiwnJ9UWvRRtPtes98ezeOlxh7zAqCyLBbKI/mTCfcqbnWqvSIpLL8U9LHg/qaIf",
+  secret_key_base: "qPxb8ntv8uMW8f6kZZ/tK8UY9EntFpdJSoKnVxe5XP2RvMkL4XMOSo3M9DB+zqUf",
   render_errors: [view: GithubReposWeb.ErrorView, accepts: ~w(json), layout: false],
   pubsub_server: GithubRepos.PubSub,
-  live_view: [signing_salt: "ZanXBpu/"]
+  live_view: [signing_salt: "1aksZ+8O"]
+
+config :github_repos, GithubRepos.Client, adapter: GithubRepos.Client
 
 # Configures Elixir's Logger
 config :logger, :console,
