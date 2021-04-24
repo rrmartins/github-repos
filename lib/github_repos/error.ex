1f3d9922955230c98dd26aa1ab5ee343720226dc
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-04-24 01:48:49 +0100

    Error module

---
+++ lib/github_repos/error.ex
@@ -0,0 +1,18 @@
+defmodule GithubRepos.Error do
+  @keys [:status, :message]
+
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(status, message) do
+    %__MODULE__{
+      status: status,
+      message: message
+    }
+  end
+
+  def build_bad_request(message), do: build(:bad_request, message)
+
+  def build_not_found(message), do: build(:not_found, message)
+end
