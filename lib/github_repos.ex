353a7f33436117423122a3444fb13bccfab016bd
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-04-24 01:47:43 +0100

    change delegates

--- lib/github_repos.ex
+++ lib/github_repos.ex
@@ -1,9 +1,11 @@
 defmodule GithubRepos do
-  @moduledoc """
-  GithubRepos keeps the contexts that define your domain
-  and business logic.
+  alias GithubRepos.Repositories.Get, as: GetRepos
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  alias GithubRepos.Users.Create, as: CreateUser
+  alias GithubRepos.Users.Get, as: GetUser
+
+  defdelegate get_repos_from_user(username), to: GetRepos, as: :from_user
+
+  defdelegate create_user(params), to: CreateUser, as: :call
+  defdelegate get_user_by_id(id), to: GetUser, as: :by_id
 end
