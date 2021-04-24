795800f52e39bf31d5b54809f5b56261912a307f
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-04-24 01:47:44 +0100

    add sandbox

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -17,6 +17,8 @@ defmodule GithubReposWeb.ChannelCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       # Import conveniences for testing with channels
@@ -29,10 +31,10 @@ defmodule GithubReposWeb.ChannelCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(GithubRepos.Repo)
+    :ok = Sandbox.checkout(GithubRepos.Repo)
 
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(GithubRepos.Repo, {:shared, self()})
+      Sandbox.mode(GithubRepos.Repo, {:shared, self()})
     end
 
     :ok
